9e40d398ea0ef1c45114e09a3653556494fb90d5
Ling0212 <93497630+Ling0212@users.noreply.github.com>
2022-01-03 14:13:00 +0000

    Update Code for Summary Data Analysis.py

--- Reproducible lines of code/Code for Summary Data Analysis.py
+++ Reproducible lines of code/Code for Summary Data Analysis.py
@@ -2,9 +2,9 @@
 
 # We obtain all the values we need to build our summary statistics table using pandas, numpy and spicy libraries.
 
-# We need to successively select the values for each variables, for either Jordan's or Lebanon's data contained in the corresponding csv file. In other words, we consecutively select one column of the csv file.
+# We need to successively select the values for each variable, for either Jordan's or Lebanon's data contained in the corresponding csv file. In other words, we consecutively select one column of the csv file.
 
-# The following lines of code allow us to compute for each variable the summary statistics values. We present for example the lines used in the case of the Total Energy Supply per capita in Leabanon, from 1990 to 2019.
+# The following lines of code allow us to compute for each variable the summary statistics values. We present for example the code used in the case of the Total Energy Supply per capita in Leabanon, from 1990 to 2019.
 
 import pandas as pd
 import numpy as np
