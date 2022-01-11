09823de6e77c588fd8946b91a26bfd6a3cbefa0e
Ling0212 <93497630+Ling0212@users.noreply.github.com>
2022-01-11 16:25:12 +0000

    Update Pearson Correlation Coefficient.py

--- Reproducible lines of code/Pearson Correlation Coefficient.py
+++ Reproducible lines of code/Pearson Correlation Coefficient.py
@@ -1,5 +1,5 @@
 # PEARSON CORRELATION COEFFICIENT IN PYTHON
-# We first import the libraries we are going to use in order to compute Pearson'r: pandas, numpy and spicy.stats. Then, we import and read the data using pandas, stored in a .csv file. We will give as an example the code analysing the correlation between the Total Electricity Generation per Capita and Life Expectancy in Lebanon, from 1990 to 2019. 
+# We first import the libraries we are going to use in order to compute Pearson'r: pandas, numpy and spicy.stats. Then, we import and read the data stored in a .csv file (using pandas). We will give as an example the code analysing the correlation between the Total Electricity Generation per Capita and Life Expectancy in Lebanon, from 1990 to 2019. 
 
 import pandas as pd
 import numpy as np
@@ -8,7 +8,7 @@ import scipy.stats
 data = pd.read_csv("lebanon_data.csv")
 data.head()
 
-# We now define our x and y variables, corresponding to the columns Total Electricity Generation per Capita and Life Expectancy in the imorted .csv file. With spicy.stats library, we calculate the Pearson correlation coefficient as presented in the following lines of code:
+# We now define our x and y variables, corresponding to the columns Total Electricity Generation per Capita and Life Expectancy in the imported .csv file. With spicy.stats library, we calculate the Pearson correlation coefficient as presented in the following lines of code:
 
 x = data['Total Electricity Generation  per Capita (GWh)']
 y = data['Life expectancy (years)']
